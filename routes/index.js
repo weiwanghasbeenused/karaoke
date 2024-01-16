9a289aa40732b63aa4800aaf28df83473fbadd5f
wei <wei@o-r-g.com>
2024-01-16 21:03:31 +0800

    update ws protocol

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res){
-    console.log(req.secure);
     let host = req.protocol === 'https' ? 'wss://' + req.get('host') : 'ws://' + req.get('host');
     res.render('index', {host: host})
 })
--- routes/player.js
+++ routes/player.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res){
-    let host = 'ws://' + req.get('host');
+    let host = req.protocol === 'https' ? 'wss://' + req.get('host') : 'ws://' + req.get('host');
     res.render('player', {host: host, YOUTUBE_API_KEY: process.env['YOUTUBE_API_KEY'], YOUTUBE_CLIENT_ID: process.env['YOUTUBE_CLIENT_ID']})
 })
 
