da7fc3fff7f3dcf47fa033f3b196b59b3c5784ba
wei <wei@o-r-g.com>
2024-01-16 20:47:25 +0800

    add conole

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res){
+    console.log(req.protocol);
     let host = req.protocol === 'https' ? 'wss://' + req.get('host') : 'ws://' + req.get('host');
     res.render('index', {host: host})
 })
